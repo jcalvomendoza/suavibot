79dd951eb1c1b2e43f50fe8c0b1944c673e0f5a7
Javier Calvo Mendoza <javiercalvomendoza@Javiers-MacBook-Pro.local>
2017-11-04 20:02:03 -0500

    Fucking mess

--- messages/index.js
+++ messages/index.js
@@ -22,39 +22,21 @@ var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
 // Make sure you add code to validate these fields
-/*var luisAppId = process.env.LuisAppId;
+var luisAppId = process.env.LuisAppId;
 var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';*/
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
 
-const LuisModel = process.env.model || 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/a4e76ead-6a36-4295-bbf5-ad248d6459c7?subscription-key=6015562483a6496aa4b663bcf624f535&spellCheck=true&verbose=true&timezoneOffset=-6.0&q=';
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
 
 // Main dialog with LUIS
-var greeting_recognizer = new builder.LuisRecognizer(LuisModel);
+var greeting_recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var greeting_intents = new builder.IntentDialog({ recognizers: [greeting_recognizer] })
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-
+*/
 .onDefault((session) => {
-    session.send('Sorry, \'%s\' is the stupidest thing I\'ve heard.', session.message.text);
-});*/
-bot.dialog('/', greeting_intents); 
-/*dialog.matches('AskAboutMe', [
-    function (session) {
-          session.beginDialog('/yourself');
-    }
-]);*/
-bot.dialog('/yourself', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    // Step 2
-    function (session, results) {
-        session.endDialog(`Hello ${results.response}!`);
-    }
-]).triggerAction({matches: 'AskAboutMe'});
-
-dialog.onDefault(builder.DialogAction.send('Sorry, \'%s\' is the stupidest thing I\'ve heard.', session.message.text));
-//bot.dialog('yourself', require('./yourself')).triggerAction({ matches : 'AskAboutMe'});   
+    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+});
 
 if (useEmulator) {
     var restify = require('restify');
@@ -67,35 +49,3 @@ if (useEmulator) {
     module.exports = { default: connector.listen() }
 }
 
-
-/*var LuisModel = process.env.model || 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/a4e76ead-6a36-4295-bbf5-ad248d6459c7?subscription-key=6015562483a6496aa4b663bcf624f535&spellCheck=true&verbose=true&timezoneOffset=-6.0&q=';
-bot.recognizer(new builder.LuisRecognizer(LuisModel));
-// Sample Dialog Code to be replaced later
-bot.dialog('/deleteAlarm', [
-    function (session, args, next) {
-        if (alarmCount() > 0) {
-            // Resolve entities passed from LUIS.
-            var title;
-            var intent = args.intent;
-            var entity = builder.EntityRecognizer.findEntity(intent.entities, 'builtin.alarm.title');
-            if (entity) {
-                // Verify its in our set of alarms.
-                title = builder.EntityRecognizer.findBestMatch(alarms, entity.entity);
-            }
-            
-            // Prompt for alarm name
-            if (!title) {
-                builder.Prompts.choice(session, 'Which alarm would you like to delete?', alarms);
-            } else {
-                next({ response: title });
-            }
-        } else {
-            session.endDialog("No alarms to delete.");
-        }
-    },
-    function (session, results) {
-        delete alarms[results.response.entity];
-        session.endDialog("Deleted the '%s' alarm.", results.response.entity);
-    }
-]).triggerAction({
-    matches: 'AskAboutMe'});*/
